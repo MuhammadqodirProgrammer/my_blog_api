09d8b8d88a9780159e2f33f6d4eef1a688ac6cba
MuhammadqodirProgrammer <159247201+komilov02@users.noreply.github.com>
2024-04-09 08:40:46 +0500

    cors

--- src/start/run.ts
+++ src/start/run.ts
@@ -4,7 +4,7 @@ import { connect } from "mongoose";
 
 const run = async (app: Application) => {
   await connect(process.env.CONNECTION_STRING);
-  console.log("Connect to database..." ,process.env.CONNECTION_STRING);
+  console.log("Connect to database..." ,process.env.CONNECTION_STRING, "hello");
 const PORT =process.env.PORT ||2002
   app.listen(PORT, () => {
     console.log(`Server running on port : ${PORT}`);
