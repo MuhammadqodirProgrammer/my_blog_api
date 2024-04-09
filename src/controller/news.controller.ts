00f708b2b5954074d1b968f2c124e02999004ba1
MuhammadqodirProgrammer <159247201+komilov02@users.noreply.github.com>
2024-04-09 09:40:26 +0500

    cors

--- src/controller/news.controller.ts
+++ src/controller/news.controller.ts
@@ -20,7 +20,7 @@ export const getNews = async (req: Request, res: Response) => {
 
         const data = await News.find();
 
-        res.status(200).json({ message: "Success ", data });
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ message: error });
     }
